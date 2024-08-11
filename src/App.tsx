ea87e26b91708bf13c492e81208c29e376092e54
Darrellson <muraddiasamidze86@gmail.com>
2024-08-12 01:53:18 +0400

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
 import { AppRoutes } from "components/routes/private-routes";
 import { ThemeProvider } from "components/theme";
-import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { BrowserRouter as Router } from "react-router-dom";
 
-const queryClient = new QueryClient();
-
 const App = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider>
-        <Router>
-          <AppRoutes />
-        </Router>
-        <ReactQueryDevtools initialIsOpen={false} />
-      </ThemeProvider>
-    </QueryClientProvider>
+    <ThemeProvider>
+      <Router>
+        <AppRoutes />
+      </Router>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </ThemeProvider>
   );
 };
 
